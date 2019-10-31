c39d7640a131534480baef4fcc709504dcee751f
ciwj <55768502+ciwj@users.noreply.github.com>
2019-10-31 18:41:46 -0400

    Update 8bitLED.py
    Better console info

--- 8bitLED.py
+++ 8bitLED.py
@@ -9,10 +9,17 @@ for pin in pins: # set all relevant pins as outputs.
     GPIO.setup(pin,GPIO.OUT)
 
 for x in range(248): # lab leader last # is 8, therefore count to 247.
+    print("Printing:", x)
+    binary = []
     for num in range(1,9): # go through all pins
         if x % 2 == 0: # if divisible by 2, set low, otherwise set high.
             GPIO.output(pins[-num],GPIO.LOW)
+            binary.insert(0, 0)
         else:
             GPIO.output(pins[-num],GPIO.HIGH)
+            binary.insert(0, 1)
         x = x // 2 # divide by two and round down
+    for binary in binary:
+        print(binary, end='')
+    print("\n")
     time.sleep(0.5)
